03a4c129aea0885b48ec0e66acbb691cf2db6d67
zhangwei <zhangweiwhim@gmail.com>
2020-03-24 11:42:02 +0800

    update flag

--- src/main/scala/com/zhangweiwhim/RepayCalc.scala
+++ src/main/scala/com/zhangweiwhim/RepayCalc.scala
@@ -1,11 +1,14 @@
 package com.zhangweiwhim
 
+import java.sql.{Connection, DriverManager}
 import java.time.LocalDate.now
 import java.util.Properties
 
+import org.apache.spark.sql
 import org.apache.spark.sql.{SaveMode, SparkSession}
 
 import scala.io.Source
+
 /**
  * Description: wk_batch
  * Created by zhangwei on 2020/2/20 13:34
@@ -24,7 +27,7 @@ object RepayCalc {
       .enableHiveSupport()
       .getOrCreate()
 
-    val query =Source.fromInputStream(this.getClass.getResourceAsStream("/query/repay_calc/common.sql")).getLines().mkString("\n")
+    val query = Source.fromInputStream(this.getClass.getResourceAsStream("/query/repay_calc/common.sql")).getLines().mkString("\n")
     // 执行日期
     spark.sql(s"set exec_date = '${exec_date}'")
     // wk表
@@ -35,12 +38,36 @@ object RepayCalc {
     properties.load(path)
     val url = properties.getProperty("mysql.db.url")
     val table = properties.getProperty("mysql.output.table")
+    val mysqlUser = properties.getProperty("mysql.user")
+    val mysqlPassword = properties.getProperty("mysql.password")
     val connectionProperties = new Properties()
-    connectionProperties.put("user", properties.getProperty("mysql.user"))
-    connectionProperties.put("password", properties.getProperty("mysql.password"))
+    connectionProperties.put("user", mysqlUser)
+    connectionProperties.put("password", mysqlPassword)
 
     // wk表写入数据库
     calcDF.write.option("truncate", "true").mode(SaveMode.Overwrite).jdbc(url, table, connectionProperties)
     spark.stop()
+    //更新标签
+    val conn: Connection = getOnlineConnection(url, mysqlUser, mysqlPassword)
+    try {
+      conn.setAutoCommit(false)
+      val sql = new StringBuilder()
+        .append("replace into wk_result (id,reuslt) values(?,?)")
+      val pstm = conn.prepareStatement(sql.toString())
+      pstm.setInt(1, 102)
+      pstm.setString(2, "ok")
+      pstm.executeBatch()
+      pstm.executeUpdate() > 0
+      conn.commit()
+    }
+    finally {
+      conn.close()
+    }
   }
+
+  def getOnlineConnection(onlineUrl: String, username: String, password: String): Connection = {
+    DriverManager.getConnection(onlineUrl, username, password)
+  }
+
+
 }
